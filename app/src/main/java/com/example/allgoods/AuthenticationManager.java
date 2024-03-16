1badaabdcf15e2aec9272b7489edd8b7baa37261
DevenTri <dtri559@aucklanduni.ac.nz>
2024-03-16 23:55:19 +1300

    Completed validator and made changes to LoginActivity.java  /*if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {                     Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();                 }*/                /* if(authenticationManager.loginAuthenticator(username.getText().toString(),password.getText().toString())){                     Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();                 }                 else {                     Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();                 }*/
                    this is the deleted code

--- app/src/main/java/com/example/allgoods/AuthenticationManager.java
+++ app/src/main/java/com/example/allgoods/AuthenticationManager.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class AuthenticationManager {
 
-    HashMap<String,String> users;
+    HashMap<String,User> users;
 
     //Password authenticator
     //adds email and password into hashmap to extract
@@ -19,19 +19,20 @@ public class AuthenticationManager {
             throw new IllegalArgumentException("You have already registered. Please Login!");
         }
         else {
-            users.put(user.getEmail(), user.getPassword());
+            users.put(user.getEmail(), user);
         }
 
     }
 
     // loginAuthenticator() function validates whether the user credintials are correct
     // inputs the user email and outputs a boolean value
-    public boolean loginAuthenticator(String email){
+    public boolean loginAuthenticator(String email, String password){
         if(!users.containsKey(email)){
             throw new IllegalArgumentException("Email is not registered. Please register!");
         }
-        if(users.){
-
+        // this if statement checks wether the users password matches the password
+        if(!users.get(email).getPassword().equals(password)){
+            throw new IllegalArgumentException("Password does not match Please try again");
         }
         return true;
     }
--- app/src/main/java/com/example/allgoods/LoginActivity.java
+++ app/src/main/java/com/example/allgoods/LoginActivity.java
@@ -11,8 +11,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     Button loginButton;
-
     Button signupText;
+    AuthenticationManager authenticationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +36,11 @@ public class LoginActivity extends AppCompatActivity {
             // login validation with data base
             // once logged in go to main activity page
             public void onClick(View view) {
-                /*if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
-                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                }*/
-                if(){
+                try {
+                    authenticationManager.loginAuthenticator(username.getText().toString(),password.getText().toString());
 
-                }
-                else {
-                    Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                } catch(IllegalArgumentException e){
+                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
